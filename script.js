01a397135d2757dba02b2ac52c185f95f45423cf
KanaKarimata <kana.lt22@gmail.com>
2022-08-01 23:42:02 +0900

    Add JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+const date = new Date();
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+document.querySelector('.date h1').innerHTML
+= months[date.getMonth()];
+
+document.querySelector('.date p').innerHTML
+= date.toDateString();
+
+let days = "";
+for (let i = 1; i <= 31; i++) {
+  days += `<div>${i}</div>`
+}
+
+console.log(months);
